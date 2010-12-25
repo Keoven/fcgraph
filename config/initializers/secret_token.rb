ecb9a0929d5eace4daa83b2e332b1308adf4f3e8
Nelvin Driz <ndriz@exist.com>
2010-12-25 21:13:25 +0800

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fcgraph::Application.config.secret_token = '392dce689aeedbdb081d27407bdd5b263cee8ed1e2840a357fc89a47badcf8945eb3168c0f60767edd8bfc97b0cf5f6471ad99a732d350d1b5962c0209655d7d'
